f491d3bd47c9eda28db90e9754499ccb8cc8ddad
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-07-23 09:28:34 +0100

    created a media file

--- qrcode_gen/views.py
+++ qrcode_gen/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-from qrcode import *
+from django.views.generic import ListView
+from qrcode_gen.models import Qr_Code
 # Create your views here.
+
+class Qr_CodeList(ListView):
+    model = Qr_Code
+    obj=Qr_Code.objects.get(id=1)
+    template_name='home.html'
+    
